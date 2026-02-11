9e5f29e21fd553eeb3b81a175bb95c97a3290f10
AQUIB IRFANI <maquib1710@gmail.com>
2026-02-11 22:47:46 +0530

    Refactor CORS and session middleware configurations
    Updated session cookie settings to include 'sameSite' attribute.

--- app.js
+++ app.js
@@ -7,26 +7,37 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: "https://expense-tracker-frontend-alpha-six.vercel.app",
-  credentials: true
-}));
-app.use(session({
-  secret: "expense-secret",
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    secure: true,
-    maxAge: 1000 * 60 * 60 * 24 // 1 day
-  }
-}));
+
+app.use(
+  cors({
+    origin: "https://expense-tracker-frontend-alpha-six.vercel.app",
+    credentials: true,
+  })
+);
+
+app.use(
+  session({
+    secret: "expense-secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: true,       
+      sameSite: "none",   
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+  })
+);
+
 connect();
+
 app.use("/api/auth", auth_routes);
 app.use("/expense", routes);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('Server is running...')
+  console.log(`Server is running on port ${PORT}`);
 });
