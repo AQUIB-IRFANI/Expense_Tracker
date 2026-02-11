e77ea0d011fc457ab1d47417ece07fa3ef1143bd
AQUIB IRFANI <maquib1710@gmail.com>
2026-02-11 22:26:10 +0530

    Protect check-auth route with authentication middleware

--- router/authroute.js
+++ router/authroute.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const { registration, login, logout } = require("../controller/authcontroller");
+const isAuthenticated = require("../middleware/authmiddleware");
+
 const routes = express.Router();
+
 routes.post("/register", registration);
 routes.post("/login", login);
 routes.post("/logout", logout);
-routes.get("/check-auth", (req, res) => {
-  if (req.session.userId) {
-    res.json({ authenticated: true });
-  } else {
-    res.status(401).json({ authenticated: false });
-  }
+
+routes.get("/check-auth", isAuthenticated, (req, res) => {
+  res.status(200).json({ authenticated: true, userId: req.session.userId });
 });
 
 module.exports = routes;
