3128d8b372331cbeae3903090ecde54b1f2b836f
AQUIB IRFANI <maquib1710@gmail.com>
2026-02-11 20:09:14 +0530

    Configure CORS and secure session cookies
    Updated CORS configuration and session cookie settings for production.

--- app.js
+++ app.js
@@ -6,23 +6,24 @@ const session = require("express-session");
 const cors = require("cors");
 require("dotenv").config();
 
-connect();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: "https://expense-tracker-frontend-alpha-six.vercel.app",
+  credentials: true
+}));
 app.use(session({
   secret: "expense-secret",
   resave: false,
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: false   // must be false in development
+    secure: true,
+    maxAge: 1000 * 60 * 60 * 24 // 1 day
   }
 }));
-app.use(cors({
-  origin: "https://expense-tracker-frontend-alpha-six.vercel.app",
-  credentials: true
-}));
+connect();
 app.use("/api/auth", auth_routes);
 app.use("/expense", routes);
 const PORT = process.env.PORT || 5000;
