de9de6416b08821032a563dcb83b27bb47c07873
AQUIB IRFANI <maquib1710@gmail.com>
2026-02-11 20:01:37 +0530

    Improve error logging for MongoDB connection

--- db/db.js
+++ db/db.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
+const mongoose = require("mongoose");
 const mongodb = () => {
   mongoose.connect(process.env.MONGO_URI)
     .then(() => {
       console.log('Connected to DB...');
 
     }).catch((err) => {
-      console.log(err);
+      console.log("MongoDB connection Error: "+ err);
 
     });
 }
