b1f06763460383a43c66e62b2924893acce434a1
AQUIB IRFANI <maquib1710@gmail.com>
2026-02-11 19:34:14 +0530

    Update CORS origin for production deployment

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(session({
   }
 }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://expense-tracker-frontend-alpha-six.vercel.app",
   credentials: true
 }));
 app.use("/api/auth", auth_routes);
